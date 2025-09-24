c80aeae99ceb59e6b800bb7771a3568d73b58907
vutrungtam619 <vutrungtam619@gmail.com>
2025-09-24 22:04:19 +0700

    add

--- models/pillarloli.py
+++ models/pillarloli.py
@@ -243,7 +243,7 @@ class PillarEncoder(nn.Module):
             pts = boundary_points[mask.cpu().numpy()]  # lấy numpy array cho points_lidar2image
             calib = batch_calibs[b.item()]
             img_pts = points_lidar2image(pts, tr_velo_to_cam=calib['T'], r0_rect=calib['R'], P2=calib['P'])
-            points2image[mask] = torch.from_numpy(img_pts).to(device)
+            points2image[mask] = torch.from_numpy(img_pts).float().to(device)
 
         image_bboxes = bounding_bboxes(points2image.cpu().numpy())
         
